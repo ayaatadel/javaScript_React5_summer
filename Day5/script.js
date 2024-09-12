410c76cf204ddc225c73f73b0a97577f229febca
ayaatadel <ayaatadel128@gmail.com>
2024-09-12 11:44:42 +0300

    day5 part 2 => splice , slice , BOM

--- Day5/script.js
+++ Day5/script.js
@@ -125,34 +125,129 @@
 // first()
 // first third fourth second   
 
-setTimeout(() => {
+// setTimeout(() => {
 
-   console.log("first");  //  first   second third
+//    console.log("first");  //  first   second third
    
 
-   setTimeout(() => {
-    console.log("second");
+//    setTimeout(() => {
+//     console.log("second");
     
-   }, 500);
-   setTimeout(() => {
-    console.log("third");
+//    }, 500);
+//    setTimeout(() => {
+//     console.log("third");
     
-   }, 1000); 
-   setTimeout(() => {
-    console.log("third");
+//    }, 1000); 
+//    setTimeout(() => {
+//     console.log("third");
     
-   }, 2000); 
-   setTimeout(() => {
-    console.log("third");
+//    }, 2000); 
+//    setTimeout(() => {
+//     console.log("third");
     
-   }, 3000); 
-   setTimeout(() => {
-    console.log("third");
+//    }, 3000); 
+//    setTimeout(() => {
+//     console.log("third");
     
-   }, 4000); 
+//    }, 4000); 
 
-}, 2000);
+// }, 2000);
 
 // call Back Hell  => problem that was result of calling setTimeOut in another SetTimeOut 
 // promises => ES6
 
+
+
+
+//********************  */
+
+// var button=document.getElementById("btn")
+// // button.addEventListener('click',()=>{
+// //    open("https://www.pexels.com/","_blank")
+// // })
+// button.addEventListener('click',()=>{
+//    open("https://www.pexels.com/","_self","width=500px,height=500px")
+// })
+
+
+
+//************   Slice , Splice   */
+
+// slice => Returns a copy of a section of an array (start,end)
+// ==> return start to end-1
+// ==> doesn't change in main array
+
+// ==>  (start , end-) => -1 => end-1
+
+// splice => add , delete , replace
+
+
+// var arr=[1,2,3,10,15]
+    
+// console.log(arr.slice(0,-2)); // -1=> start to end-1   => 1
+// console.log(arr);
+
+
+// splice   1- add  (start,0,element)
+//          2- replace (start,1,elements)
+// [1,2,3,10,15]
+// console.log("arrray before splice",arr);
+// [1,2,3,10,15]
+// arr.splice(1,0,20)      // add
+// [1, 20, 2, 3, 10, 15]   // add
+// arr.splice(1,0,"ayaat","nada")  
+// [1, "ayaat","nada",20, 2, 3, 10, 15]
+
+// arr.splice(1,1,"ayaat","nada")  // replace => index (1)=>"ayaat nada"
+
+
+// arr.splice(0,3) // delete (start of delete, number of elemnts that delete)
+// var arr=[1,2,3,10,15,"ayaat","nada",true,false,2.2,'hh','xx']
+// // arr.splice(-1,3)   // end, count-1  => 
+//    //  [1, 2, 3, 10, 15, 'ayaat', 'nada', true, false]
+// // [1,2,3,10,15,"ayaat","nada",true,false,2.2]
+// console.log(arr);
+
+// arr.splice(-3,4)
+// console.log("arrray after splice",arr);
+
+
+//  BOM  => Browser Object Model   => browser => window
+// select using id
+// var window;
+// // open page
+// btn.addEventListener("click",()=>{
+//    // console.log("hello");
+//    pageOpen= open("https://www.pexels.com","_blank")
+   
+
+// })
+// // close open page
+// cls.addEventListener("click",()=>{
+//    pageOpen.close()
+// })
+
+// jp.addEventListener("click",()=>{
+//     window.location.href="https://www.javatpoint.com/browser-object-model"
+//   })
+
+// close current window
+// cls.addEventListener("click",()=>{
+//    window.close()
+// })
+var pageOpen;
+btn.addEventListener("click",()=>{
+  // console.log("hello");
+  pageOpen= open("https://www.pexels.com","_self")
+  
+
+})
+
+// console.log(window.history.back().url);  // previous
+// console.log(window.history.forward().url); // next
+// window.history.go(1) => 1 => forward , -1 => back
+
+// jp.addEventListener("click",()=>{
+// window.history.go(1)
+
+// })
---
+++ Day5/test.js
@@ -0,0 +1,8 @@
+var arr=[1,2,3,10,15,"ayaat","nada",true,false,2.2,'hh','xx']
+// arr.splice(-1,3)   // end, count-1  => 
+   //  [1, 2, 3, 10, 15, 'ayaat', 'nada', true, false]
+// [1,2,3,10,15,"ayaat","nada",true,false,2.2]
+console.log(arr);
+
+arr.splice(-3,4)
+console.log("arrray after splice",arr);
